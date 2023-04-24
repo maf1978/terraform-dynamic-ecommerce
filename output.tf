ea855e44de0520f2cb2fe30446b51d37aa31fb14
marcos Ferrero <marcos.ferrero001@mymdc.net>
2023-04-24 18:50:23 -0400

    Added output commit

---
+++ output.tf
@@ -0,0 +1,12 @@
+output "vpc_id" {
+  value = aws_vpc.vpc.id
+}
+
+output "public_subnet_az1_id" {
+  value = aws_subnet.public_subnet_az1.data
+    
+}
+
+output "website_url" {
+  value = join ("", ["https://", var.record_name, ".", var.domain_name])
+}
